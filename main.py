fa50c3f506999dc74866ed187c3fe22d1b9cdeda
Sadeeq Bello <49214583+Lekzyboi@users.noreply.github.com>
2025-07-09 18:24:08 -0300

    data validation completed

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from src.mlproject import logger
 from src.mlproject.pipelines.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.mlproject.pipelines.data_validation_pipeline import DataValidationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-if __name__ == "__main__":
-    try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        data_ingestion = DataIngestionTrainingPipeline()
-        data_ingestion.initiate_data_ingestion()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
-    except Exception as e:
-        logger.exception(e)
-        raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation._validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+    
+    
+    
