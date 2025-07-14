9fcae8b9411442a04bdb6c908d833cbffc4d4612
Sadeeq Bello <49214583+Lekzyboi@users.noreply.github.com>
2025-07-14 07:44:14 -0300

    data_transformation completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.mlproject import logger
 from src.mlproject.pipelines.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.mlproject.pipelines.data_validation_pipeline import DataValidationTrainingPipeline
+from src.mlproject.pipelines.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -20,6 +21,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation._validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformationtion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataTransformationTrainingPipeline()
+    data_ingestion.initiate_data_transformation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.exception(e)
